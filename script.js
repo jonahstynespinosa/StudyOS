450d1d6326f473fe37117d1fff268ec4730c2422
Jonah Styn-Espinosa <jonah.avenger@gmail.com>
2026-03-05 23:53:17 -0500

    Here is a simple api push using js

---
+++ script.js
@@ -0,0 +1,39 @@
+const btn = document.getElementById("load-assignment")
+const dataSet = document.getElementById("dataSet")
+
+let assignments = []
+
+
+async function loadAssignments() {
+  try {
+  dataSet.innerHTML = "<p>Loading...</p>"
+
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos?_limit=10")
+
+    if(!res.ok) {
+      throw new Error("Failed to fetch assignments")
+    }
+
+    assignments = await res.json()
+    displayAssignments(assignments)
+  } catch (error) {
+    dataSet.innerHTML = `<p class="error">${error.message}</p>`
+  }
+}
+
+btn.addEventListener("click", loadAssignments) 
+
+function displayAssignments(assignments) {
+  dataSet.innerHTML = ""
+  
+  assignments.forEach(assignment => {
+    const assignmentDiv = document.createElement("div")
+    assignmentDiv.classList.add("data-item")
+
+    assignmentDiv.innerHTML = `
+      <h3>${assignment.title}</h3>
+      <p>${assignment.completed ? "Completed" : "Not completed"}</p>
+    `
+    dataSet.appendChild(assignmentDiv)
+  })
+}
