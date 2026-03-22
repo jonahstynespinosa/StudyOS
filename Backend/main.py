9883258fb208eaff645f7637c88a2c1540308ec5
Jonah Styn-Espinosa <jonah.avenger@gmail.com>
2026-03-22 09:52:58 -0400

    I added a main backend page with python and I am currently using FastAPI to load the backend

---
+++ Backend/main.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, Field
+
+from tasks import add_task, get_tasks, complete_task,delete_task 
+
+app = FastAPI(title="StudyOS API")
+
+class TaskCreate(BaseModel):
+    title: str = Field(min_length=1, max_length=100)
+
+
+@app.get("/")
+def root(): 
+    return {"message": "studyOS API is running"}
+
+@app.get("/tasks")
+def read_tasks():
+    return {"tasks": get_tasks()}
+
+
+@app.post("/tasks", status_code=201)
+def root():
+    return {"message": "studyOS API is running"}
+
+@app.post("/tasks")
+def create_task(task: TaskCreate):
+    new_task = add_task(task.title)
+    return {"message": "Task created successfully", "task": new_task}
+
+@app.put("/tasks/{task_id}")
+def mark_task_complete(task_id: int):
+    updated_task = complete_task(task_id)
+
+    if updated_task is None:
+        raise HTTPException(status_code=404, detail="Task not found")
+    
+    return {"message": "Task completed", "task": updated_task}
+
+@app.delete("/tasks/{task_id}")
+def remove_task(task_id: int):
+    deleted_task = delete_task(task_id)
+
+    if deleted_task is None: 
+      raise HTTPException(status_code=404, detail="Task not found")
+    
+    return {"message": "Task deleted", "task": deleted_task}
--- Backend/tasks.py
+++ Backend/tasks.py
@@ -18,12 +18,12 @@ def complete_task(task_id):
   for task in tasks:
     if task['id'] == task_id:
       task['completed'] = True
-      return 
+      return None
   print(f"Task with id {task_id} not found.")
 
 def delete_task(task_id):
   for i, task in enumerate(tasks): 
     if task['id'] == task_id:
       tasks.pop(i)
-      return
+      return None
   print(f"Task with id {task_id} not found.")
