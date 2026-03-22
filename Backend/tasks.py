362e2af805c4fb0afcca3928a7d81e0b3b8b0ac7
Jonah Styn-Espinosa <jonah.avenger@gmail.com>
2026-03-21 23:45:08 -0400

    Here is the start to learning python for backend dev

---
+++ Backend/tasks.py
@@ -0,0 +1,29 @@
+tasks = []
+next_id = 1
+
+def add_task(title):
+  global next_id
+  task = {
+    'id': next_id,
+    'title': title,
+    'completed': False
+  }
+  tasks.append(task)
+  next_id += 1
+
+def get_tasks():
+  return tasks
+
+def complete_task(task_id):
+  for task in tasks:
+    if task['id'] == task_id:
+      task['completed'] = True
+      return 
+  print(f"Task with id {task_id} not found.")
+
+def delete_task(task_id):
+  for i, task in enumerate(tasks): 
+    if task['id'] == task_id:
+      tasks.pop(i)
+      return
+  print(f"Task with id {task_id} not found.")
