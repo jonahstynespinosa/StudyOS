e09c22c4b566774b2a3e2ac1b1575df374020ea2
Jonah Styn-Espinosa <jonah.avenger@gmail.com>
2026-03-24 15:19:29 -0400

    He is the fixed problem

--- Backend/main.py
+++ Backend/main.py
@@ -1,17 +1,24 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, Field
 
-from tasks import add_task, get_tasks, complete_task,delete_task 
+from tasks import add_task, get_tasks, delete_task, update_task
 
 app = FastAPI(title="StudyOS API")
+
 
 class TaskCreate(BaseModel):
     title: str = Field(min_length=1, max_length=100)
 
 
+class TaskUpdate(BaseModel):
+    title: str
+    completed: bool
+
+
 @app.get("/")
-def root(): 
+def root():
     return {"message": "studyOS API is running"}
+
 
 @app.get("/tasks")
 def read_tasks():
@@ -19,36 +26,10 @@ def read_tasks():
 
 
 @app.post("/tasks", status_code=201)
-def root():
-    return {"message": "studyOS API is running"}
-
-@app.post("/tasks")
 def create_task(task: TaskCreate):
     new_task = add_task(task.title)
     return {"message": "Task created successfully", "task": new_task}
 
-@app.put("/tasks/{task_id}")
-def mark_task_complete(task_id: int):
-    updated_task = complete_task(task_id)
-
-    if updated_task is None:
-        raise HTTPException(status_code=404, detail="Task not found")
-    
-    return {"message": "Task completed", "task": updated_task}
-
-@app.delete("/tasks/{task_id}")
-def remove_task(task_id: int):
-    deleted_task = delete_task(task_id)
-
-    if deleted_task is None: 
-      raise HTTPException(status_code=404, detail="Task not found")
-    
-    return {"message": "Task deleted", "task": deleted_task}
-
-
-class TaskUpdate(BaseModel):
-    title: str
-    completed: bool
 
 @app.put("/tasks/{task_id}")
 def update_task_endpoint(task_id: int, updated: TaskUpdate):
@@ -56,5 +37,15 @@ def update_task_endpoint(task_id: int, updated: TaskUpdate):
 
     if task is None:
         raise HTTPException(status_code=404, detail="Task not found")
-    
+
     return {"message": "Task updated", "task": task}
+
+
+@app.delete("/tasks/{task_id}")
+def remove_task(task_id: int):
+    deleted_task = delete_task(task_id)
+
+    if deleted_task is None:
+        raise HTTPException(status_code=404, detail="Task not found")
+
+    return {"message": "Task deleted", "task": deleted_task}
